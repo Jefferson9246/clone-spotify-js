3d6eb394f22c49f1332f4cb6a96f53fbcc221c73
Jefferson <jeffersonrodrigomoreira@gmail.com>
2022-03-20 20:28:35 -0400

    eventos e inserindo musicas

--- server/index.js
+++ server/index.js
@@ -5,3 +5,9 @@ import { logger } from './util.js'
 // console.log(config)
 server.listen(3000) //quando o server na porta 3000 estiver ouvindo abre console
 .on('lintening' , () => logger.info(`server running ${config.port}`))
+
+// impede que a aplicação caia, caso um erro não tratado aconteça!
+// uncaughtException => throw
+// unhandledRejection => Promises
+process.on('uncaughtException', (error) => logger.error(`unhandledRejection happened: ${error.stack || error }`))
+process.on('unhandledRejection', (error) => logger.error(`unhandledRejection happened: ${error.stack || error }`))
